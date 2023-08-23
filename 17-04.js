022f027514e8d58f555f4c9e3c0936240cc6df0c
HyeongminJo <zzz659@naver.com>
2023-08-23 13:32:57 +0900

    230823
    230823

--- 17-04.js
+++ 17-04.js
@@ -5,11 +5,13 @@ function Circle(radius) {
     this.getDiameter = function() {
         return 2 * this.radius;
     };
+    return 100;
 }
 
 // 인스턴스 생성
 const circle1 = new Circle(5); // 반지름이 5인 Circle 객체를 생성
 const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
 
+console.log(circle1);
 console.log(circle1.getDiameter()); //10
 console.log(circle2.getDiameter()); //20
